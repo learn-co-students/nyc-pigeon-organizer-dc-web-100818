7b9a9b734ff55754d03242a8efb27c952a735ad5
Iman Jawad <imanj12@gmail.com>
2018-10-01 20:57:16 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized = {}
+  data.each do |key, hash|
+    hash.each do |value, names|
+      names.each do |name|
+        organized[name] ||= {}
+        organized[name][key] ||= []
+        organized[name][key] << value.to_s
+      end
+    end
+  end
+  organized
 end
