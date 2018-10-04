fe469ff9d97c045c165d6a48658af0372606991d
cd <coraldarling@outlook.com>
2018-10-04 22:19:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  goodfeathers = {}
+  data.each do |attribute, options_list|
+    options_list.each do |option, name_array|
+      name_array.each do |name|
+        if !goodfeathers[name]
+          goodfeathers[name] = {}
+        end
+        if !goodfeathers[name][attribute]
+          goodfeathers[name][attribute] = [option.to_s]
+        elsif attribute == :color
+          goodfeathers[name][:color] << option.to_s
+        end
+      end
+    end
+  end
+  goodfeathers
 end
