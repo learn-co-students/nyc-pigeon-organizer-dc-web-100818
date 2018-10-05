51c82b8de2896c38d8cba92dd1046bc27a2a70de
Ana Harris <vana1990@gmail.com>
2018-10-05 22:03:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,21 +10,19 @@ def nyc_pigeon_organizer(data)
   end
   names = names.flatten.uniq
   names.each do |i|
-    binding.pry
     pigeon_list[i] = {
       :color => [],
       :gender => [],
-      :lives => []
-    } 
+      :lives => []}
   end
+  pigeon_list.each do |name, p_data|
+    data.each do |feature, feat_hash|
+      feat_hash.each do |feat, arr|
+        if arr.include?(name)
+          pigeon_list[name][feature] << feat.to_s
+        end
+      end
+    end
+  end
+  return pigeon_list
 end
-
-
-#pigeon_list.each do |name, arr|
-   # data.each do |feature, feat_hash|
-    #  feat_hash.each do |k, v|
-       # if v.include?(name)
-        #  pigeon_list[name][feature] << v.to_s
-     # end
-    #end
-  #end
